6fd2cd56f42ae94da3ce98dd1d2296bbdbb20447
Y. T. Chung <zonyitoo@gmail.com>
2014-11-15 09:52:40 +0800

    fixed build with nightly

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@ extern crate libc;
 use std::fmt::Show;
 use std::fmt;
 use std::io::BufReader;
-use std::num::SignedInt;
+use std::num::Signed;
 use std::string::String;
 use std::time::Duration;
 
