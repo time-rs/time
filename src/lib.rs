73aa20a11fa517a46ae3a56df7d3d6aa6346c90f
Alex Crichton <alex@alexcrichton.com>
2014-12-11 19:58:04 -0800

    Add doc url to the crate

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,9 @@
 
 //! Simple time handling.
 
-#![feature(phase)]
+#![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
+       html_favicon_url = "http://www.rust-lang.org/favicon.ico",
+       html_root_url = "http://doc.rust-lang.org/time/")]
 
 #[cfg(test)] #[phase(plugin, link)] extern crate log;
 
