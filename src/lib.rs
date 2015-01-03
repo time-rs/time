e59dbdd87464c05cfb505a81d8964574083d0834
Alex Crichton <alex@alexcrichton.com>
2015-01-03 13:45:56 -0800

    Fix tests for serialization

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@
 
 //! Simple time handling.
 
-#![feature(phase)]
+#![feature(phase, old_orphan_check)]
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "http://www.rust-lang.org/favicon.ico",
        html_root_url = "http://doc.rust-lang.org/time/")]
