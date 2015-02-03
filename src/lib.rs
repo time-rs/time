432268ef21eeb34c44a97f9294f8e2d9c94c795b
Alex Crichton <alex@alexcrichton.com>
2015-02-03 08:21:59 -0800

    Update to using feature attributes

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,8 @@
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "http://www.rust-lang.org/favicon.ico",
        html_root_url = "http://doc.rust-lang.org/time/")]
-#![allow(unstable)]
+#![feature(io, core, collections, std_misc, libc)]
+#![cfg_attr(test, feature(test, os))]
 
 #[cfg(test)] #[macro_use] extern crate log;
 
