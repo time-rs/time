8bf840d2955867d26014ece48dc1b012c7047123
Alex Crichton <alex@alexcrichton.com>
2015-02-20 08:24:30 -0800

    Update to rust master

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "http://www.rust-lang.org/favicon.ico",
        html_root_url = "http://doc.rust-lang.org/time/")]
-#![feature(io, core, collections, std_misc)]
+#![feature(old_io, core, collections, std_misc)]
 #![cfg_attr(test, deny(warnings))]
 #![cfg_attr(test, feature(test, env))]
 
