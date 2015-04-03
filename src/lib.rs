066e265c1f021d4589c04c2958f08cc84b5b7bb9
Loren Van Spronsen <lovans@microsoft.com>
2015-04-02 17:03:26 -0700

    Fix the conversion from filetime to ns

--- src/lib.rs
+++ src/lib.rs
@@ -216,7 +216,7 @@ pub fn get_time() -> Timespec {
         // hectonanosecond (100-nanosecond) intervals since 1601-01-01T00:00:00Z.
         // http://support.microsoft.com/kb/167296/en-us
         let ns_since_1601 = (((time.dwHighDateTime as u64) << 32) |
-                             ((time.dwLowDateTime  as u64) <<  0)) / 10;
+                             ((time.dwLowDateTime  as u64) <<  0)) / 100;
         let ns_since_1970 = ns_since_1601 - NANOSECONDS_FROM_1601_TO_1970;
 
         ((ns_since_1970 / 1000000) as i64,
