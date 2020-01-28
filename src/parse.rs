899c9e223ac8c192abc1a4b9891cef45ffee6c73
Josh Stone <jistone@redhat.com>
2020-01-28 10:54:04 -0800

    Allow deprecated trim_left_matches

--- src/parse.rs
+++ src/parse.rs
@@ -326,6 +326,7 @@ fn match_digits_i64(ss: &mut &str, min_digits : usize, max_digits: usize, ws: bo
     let mut value : i64 = 0;
     let mut n = 0;
     if ws {
+        #[allow(deprecated)] // use `trim_start_matches` starting in 1.30
         let s2 = ss.trim_left_matches(" ");
         n = ss.len() - s2.len();
         if n > max_digits { return None }
