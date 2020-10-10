ea1ca2e5bc3fde3033517d2443cdbb08319dfb30
Emil Lundberg <emil@emlun.se>
2020-10-10 20:08:38 +0200

    Add benchmark for rand feature

---
+++ time-benchmarks/benches/rand.rs
@@ -0,0 +1,24 @@
+use bench_util::setup_benchmark;
+use criterion::BatchSize;
+use rand::Rng;
+use time::{Date, Duration, OffsetDateTime, PrimitiveDateTime, Time, UtcOffset, Weekday};
+
+setup_benchmark! {
+    "Random",
+
+    fn rng(ben: &mut Bencher) {
+        ben.iter_batched_ref(
+            || rand::rngs::mock::StepRng::new(0, 1),
+            |rng| {
+                let _ = rng.gen::<Time>();
+                let _ = rng.gen::<Date>();
+                let _ = rng.gen::<UtcOffset>();
+                let _ = rng.gen::<PrimitiveDateTime>();
+                let _ = rng.gen::<OffsetDateTime>();
+                let _ = rng.gen::<Duration>();
+                let _ = rng.gen::<Weekday>();
+            },
+            BatchSize::SmallInput
+        );
+    }
+}
