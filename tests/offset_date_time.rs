896c373343262e671cd62bc653a557ddf7317cad
Shogo Otake <shogo.otake@gmail.com>
2021-01-09 10:44:58 +0900

    Correct Unix timestamp implementation

--- tests/offset_date_time.rs
+++ tests/offset_date_time.rs
@@ -123,6 +123,10 @@ fn timestamp() {
             .timestamp(),
         3_600,
     );
+    assert_eq!(
+        (OffsetDateTime::unix_epoch() - 1.nanoseconds()).timestamp(),
+        -1
+    );
 }
 
 #[test]
