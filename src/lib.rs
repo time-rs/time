20079038a6e1c7f2bcf2f37f9df5d3321277b9ab
Jacob Pratt <jacob@jhpratt.dev>
2021-01-17 03:42:03 -0500

    Remove macros flag
    By unconditionally relying on the macros, build time is minimally
    impacted, while users will be able to use more performant capabilities
    out of the box.

--- src/lib.rs
+++ src/lib.rs
@@ -20,10 +20,6 @@
 //!
 //!   Enables a number of features that require the ability to dynamically allocate memory.
 //!
-//! - `macros`
-//!
-//!   Enables macros that provide compile-time verification of values and intuitive syntax.
-//!
 //! - `local-offset` (_implicitly enables `std`_)
 //!
 //!   This feature enables a number of methods that allow obtaining the system's UTC offset.
@@ -36,6 +32,10 @@
 //!
 //!   Note that enabling this feature has some costs, as it means forgoing some optimizations.
 //!   Ambiguities may be introduced when parsing that would not otherwise exist.
+//!
+//!   If you are using this feature, **please leave a comment**
+//!   [on this discussion](https://github.com/time-rs/time/discussions/306) with your use case. If
+//!   there is not sufficient demand for this feature, it will be dropped in a future release.
 //!
 //! - `serde`
 //!
@@ -182,6 +182,8 @@ mod hack;
 #[cfg(feature = "std")]
 #[cfg_attr(__time_03_docs, doc(cfg(feature = "std")))]
 mod instant;
+/// Macros to construct statically known values.
+pub mod macros;
 /// The [`OffsetDateTime`] struct and its associated `impl`s.
 mod offset_date_time;
 /// The [`PrimitiveDateTime`] struct and its associated `impl`s.
@@ -203,103 +205,6 @@ mod utc_offset;
 pub mod util;
 /// Days of the week.
 mod weekday;
-/// Macros to construct statically known values.
-#[cfg(feature = "macros")]
-#[cfg_attr(__time_03_docs, doc(cfg(feature = "macros")))]
-pub mod macros {
-    /// Construct a [`Date`](crate::Date) with a statically known value.
-    ///
-    /// The resulting expression can be used in `const` or `static` declarations.
-    ///
-    /// Three formats are supported: year-week-weekday, year-ordinal, and year-month-day.
-    ///
-    /// ```rust
-    /// # use time::{Date, Weekday::*};
-    /// # use time_macros::date;
-    /// assert_eq!(
-    ///     date!("2020-W01-3"),
-    ///     Date::from_iso_week_date(2020, 1, Wednesday)?
-    /// );
-    /// assert_eq!(date!("2020-001"), Date::from_ordinal_date(2020, 1)?);
-    /// assert_eq!(date!("2020-01-01"), Date::from_calendar_date(2020, 1, 1)?);
-    /// # Ok::<_, time::Error>(())
-    /// ```
-    pub use time_macros::date;
-    /// Construct a [`PrimitiveDateTime`] or [`OffsetDateTime`] with a statically known value.
-    ///
-    /// The resulting expression can be used in `const` or `static` declarations.
-    ///
-    /// The syntax accepted by this macro is the same as [`date!`] and [`time!`], with an
-    /// optional [`offset!`], all space-separated. If an [`offset!`] is provided, the resulting
-    /// value will be an [`OffsetDateTime`]; otherwise it will be a [`PrimitiveDateTime`].
-    ///
-    /// [`date!`]: crate::macros::date
-    /// [`time!`]: crate::macros::time
-    /// [`offset!`]: crate::macros::offset
-    /// [`OffsetDateTime`]: crate::OffsetDateTime
-    /// [`PrimitiveDateTime`]: crate::PrimitiveDateTime
-    pub use time_macros::datetime;
-    /// Construct a [`UtcOffset`](crate::UtcOffset) with a statically known value.
-    ///
-    /// The resulting expression can be used in `const` or `static` declarations.
-    ///
-    /// A sign and the hour must be provided; minutes and seconds default to zero. `UTC` (both
-    /// uppercase and lowercase) is also allowed.
-    ///
-    /// ```rust
-    /// # use time::UtcOffset;
-    /// # use time_macros::offset;
-    /// assert_eq!(offset!("UTC"), UtcOffset::from_hms(0, 0, 0)?);
-    /// assert_eq!(offset!("utc"), UtcOffset::from_hms(0, 0, 0)?);
-    /// assert_eq!(offset!("+0"), UtcOffset::from_hms(0, 0, 0)?);
-    /// assert_eq!(offset!("+1"), UtcOffset::from_hms(1, 0, 0)?);
-    /// assert_eq!(offset!("-1"), UtcOffset::from_hms(-1, 0, 0)?);
-    /// assert_eq!(offset!("+1:30"), UtcOffset::from_hms(1, 30, 0)?);
-    /// assert_eq!(offset!("-1:30"), UtcOffset::from_hms(-1, -30, 0)?);
-    /// assert_eq!(offset!("+1:30:59"), UtcOffset::from_hms(1, 30, 59)?);
-    /// assert_eq!(offset!("-1:30:59"), UtcOffset::from_hms(-1, -30, -59)?);
-    /// assert_eq!(offset!("+23:59:59"), UtcOffset::from_hms(23, 59, 59)?);
-    /// assert_eq!(offset!("-23:59:59"), UtcOffset::from_hms(-23, -59, -59)?);
-    /// # Ok::<_, time::Error>(())
-    /// ```
-    pub use time_macros::offset;
-    /// Construct a [`Time`](crate::Time) with a statically known value.
-    ///
-    /// The resulting expression can be used in `const` or `static` declarations.
-    ///
-    /// Hours and minutes must be provided, while seconds defaults to zero. AM/PM is allowed
-    /// (either uppercase or lowercase). Any number of subsecond digits may be provided (though
-    /// any past nine will be discarded).
-    ///
-    /// All components are validated at compile-time. An error will be raised if any value is
-    /// invalid.
-    ///
-    /// ```rust
-    /// # use time::Time;
-    /// # use time_macros::time;
-    /// assert_eq!(time!("0:00"), Time::from_hms(0, 0, 0)?);
-    /// assert_eq!(time!("1:02:03"), Time::from_hms(1, 2, 3)?);
-    /// assert_eq!(
-    ///     time!("1:02:03.004_005_006"),
-    ///     Time::from_hms_nano(1, 2, 3, 4_005_006)?
-    /// );
-    /// assert_eq!(time!("12:00 am"), Time::from_hms(0, 0, 0)?);
-    /// assert_eq!(time!("1:02:03 am"), Time::from_hms(1, 2, 3)?);
-    /// assert_eq!(
-    ///     time!("1:02:03.004_005_006 am"),
-    ///     Time::from_hms_nano(1, 2, 3, 4_005_006)?
-    /// );
-    /// assert_eq!(time!("12:00 pm"), Time::from_hms(12, 0, 0)?);
-    /// assert_eq!(time!("1:02:03 pm"), Time::from_hms(13, 2, 3)?);
-    /// assert_eq!(
-    ///     time!("1:02:03.004_005_006 pm"),
-    ///     Time::from_hms_nano(13, 2, 3, 4_005_006)?
-    /// );
-    /// # Ok::<_, time::Error>(())
-    /// ```
-    pub use time_macros::time;
-}
-
 pub use crate::time::Time;
 pub use date::Date;
 pub use duration::Duration;
