4b740a25075ca81ebfd5654588b1699faf89febf
Adam <classygopher@gmail.com>
2021-05-17 18:51:51 +0100

    Remove WINAPI dependency, replace with FFI calls

--- src/utc_offset.rs
+++ src/utc_offset.rs
@@ -448,12 +448,43 @@ fn local_offset_at(datetime: OffsetDateTime) -> Option<UtcOffset> {
     {
         use core::mem::MaybeUninit;
 
-        use winapi::shared::minwindef::FILETIME;
-        use winapi::um::minwinbase::SYSTEMTIME;
-        use winapi::um::timezoneapi::{SystemTimeToFileTime, SystemTimeToTzSpecificLocalTime};
+        // ffi: WINAPI FILETIME struct
+        #[repr(C)]
+        #[allow(non_snake_case)]
+        struct FileTime {
+            dwLowDateTime: u32,
+            dwHighDateTime: u32,
+        }
+
+        // ffi: WINAPI SYSTEMTIME struct
+        #[repr(C)]
+        #[allow(non_snake_case)]
+        struct SystemTime {
+            wYear: u16,
+            wMonth: u16,
+            wDayOfWeek: u16,
+            wDay: u16,
+            wHour: u16,
+            wMinute: u16,
+            wSecond: u16,
+            wMilliseconds: u16,
+        }
+
+        #[link(name = "Kernel32")]
+        extern "system" {
+            // https://docs.microsoft.com/en-us/windows/win32/api/timezoneapi/nf-timezoneapi-systemtimetofiletime
+            fn SystemTimeToFileTime(lpSystemTime: *const SystemTime, lpFileTime: *mut FileTime) -> i32;
+
+            // https://docs.microsoft.com/en-us/windows/win32/api/timezoneapi/nf-timezoneapi-systemtimetotzspecificlocaltime
+            fn SystemTimeToTzSpecificLocalTime(
+                lpTimeZoneInformation: *const std::ffi::c_void, // We only pass `nullptr` here
+                lpUniversalTime: *const SystemTime,
+                lpLocalTime: *mut SystemTime,
+            ) -> i32;
+        }
 
         /// Convert a `SYSTEMTIME` to a `FILETIME`. Returns `None` if any error occurred.
-        fn systemtime_to_filetime(systime: &SYSTEMTIME) -> Option<FILETIME> {
+        fn systemtime_to_filetime(systime: &SystemTime) -> Option<FileTime> {
             let mut ft = MaybeUninit::uninit();
 
             // Safety: `SystemTimeToFileTime` is thread-safe. We are only assuming initialization if
@@ -470,17 +501,17 @@ fn local_offset_at(datetime: OffsetDateTime) -> Option<UtcOffset> {
         }
 
         /// Convert a `FILETIME` to an `i64`, representing a number of seconds.
-        fn filetime_to_secs(filetime: &FILETIME) -> i64 {
+        fn filetime_to_secs(filetime: &FileTime) -> i64 {
             /// FILETIME represents 100-nanosecond intervals
             const FT_TO_SECS: i64 = 10_000_000;
             ((filetime.dwHighDateTime as i64) << 32 | filetime.dwLowDateTime as i64) / FT_TO_SECS
         }
 
         /// Convert an [`OffsetDateTime`] to a `SYSTEMTIME`.
-        fn offset_to_systemtime(datetime: OffsetDateTime) -> SYSTEMTIME {
+        fn offset_to_systemtime(datetime: OffsetDateTime) -> SystemTime {
             let (_, month, day_of_month) =
                 datetime.to_offset(UtcOffset::UTC).date().to_calendar_date();
-            SYSTEMTIME {
+            SystemTime {
                 wYear: datetime.year() as _,
                 wMonth: month as _,
                 wDay: day_of_month as _,
