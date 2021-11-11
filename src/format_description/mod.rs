fff7f8084709440343c5dc0f34f8b5531dca1f47
Shadlock0133 <tomek-kubel2@wp.pl>
2021-11-11 05:58:24 +0100

    Add example to rfc3339's docs (#391)

--- src/format_description/mod.rs
+++ src/format_description/mod.rs
@@ -39,6 +39,26 @@ mod helper {
 /// Well-known formats, typically RFCs.
 pub mod well_known {
     /// The format described in [RFC 3339](https://tools.ietf.org/html/rfc3339#section-5.6).
+    ///
+    /// Format example: 1985-04-12T23:20:50.52Z
+    ///
+    /// ```rust
+    /// # use time::{format_description::well_known::Rfc3339, macros::datetime, OffsetDateTime};
+    /// assert_eq!(
+    ///     OffsetDateTime::parse("1985-04-12T23:20:50.52Z", &Rfc3339)?,
+    ///     datetime!(1985-04-12 23:20:50.52 +00:00)
+    /// );
+    /// # Ok::<_, time::Error>(())
+    /// ```
+    ///
+    /// ```rust
+    /// # use time::{format_description::well_known::Rfc3339, macros::datetime};
+    /// assert_eq!(
+    ///     datetime!(1985-04-12 23:20:50.52 +00:00).format(&Rfc3339)?,
+    ///     "1985-04-12T23:20:50.52Z"
+    /// );
+    /// # Ok::<_, time::Error>(())
+    /// ```
     #[derive(Debug, Clone, Copy, PartialEq, Eq)]
     pub struct Rfc3339;
 }
