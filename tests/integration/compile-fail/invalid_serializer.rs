1911991e05d1416e8b6e609b18f08a300775b99f
Zachary Newman <z@znewman.net>
2021-11-12 18:45:17 -0500

    Improve coverage of compile-fail test

--- tests/integration/compile-fail/invalid_serializer.rs
+++ tests/integration/compile-fail/invalid_serializer.rs
@@ -1,9 +1,21 @@
-use time::macros::declare_format_string_offset_date_time;
+use time::macros::{
+    declare_format_string_offset_date_time, declare_format_string_primitive_date_time,
+};
 
+declare_format_string_offset_date_time!(); // unexpected end of input
 declare_format_string_offset_date_time!("[year] [month]"); // missing ident
 declare_format_string_offset_date_time!(my_format); // missing string format
 declare_format_string_offset_date_time!(my_format "[year] [month]"); // missing comma
+declare_format_string_offset_date_time!(my_format : "[year] [month]"); // not a comma
 declare_format_string_offset_date_time!(my_format, "[bad]"); // bad component name
 declare_format_string_offset_date_time!(my_format, not_string); // string format wrong type
+
+declare_format_string_primitive_date_time!(); // unexpected end of input
+declare_format_string_primitive_date_time!("[year] [month]"); // missing ident
+declare_format_string_primitive_date_time!(my_format); // missing string format
+declare_format_string_primitive_date_time!(my_format "[year] [month]"); // missing comma
+declare_format_string_primitive_date_time!(my_format : "[year] [month]"); // not a comma
+declare_format_string_primitive_date_time!(my_format, "[bad]"); // bad component name
+declare_format_string_primitive_date_time!(my_format, not_string); // string format wrong type
 
 fn main() {}
