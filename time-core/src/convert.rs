f4ca11a3b3f6a3e2b57e4afbad3adffe1431702b
Jacob Pratt <jacob@jhpratt.dev>
2026-02-05 23:46:57 -0500

    Declare unit types separately from trait impls

--- time-core/src/convert.rs
+++ time-core/src/convert.rs
@@ -44,18 +44,23 @@ where
     T::VALUE
 }
 
-/// Declare and implement `Per` for all relevant types. Identity implementations are automatic.
+/// Declare all unit types.
+macro_rules! declare_types {
+    ($($t:ident ($str:literal))+) => {$(
+        #[doc = concat!("A unit of time representing exactly one ", $str, ".")]
+        #[derive(Debug, Clone, Copy)]
+        pub struct $t;
+    )+};
+}
+
+/// Implement `Per` for all relevant types. Identity implementations are automatic.
 macro_rules! impl_per {
-    ($($t:ident ($str:literal) per {$(
+    ($($t:ident per {$(
         $larger:ident : [$default_output:ty]
 
         $($int_output:ty)|+ = $int_value:expr;
         $($float_output:ty)|+ = $float_value:expr;
     )+})*) => {$(
-        #[doc = concat!("A unit of time representing exactly one ", $str, ".")]
-        #[derive(Debug, Clone, Copy)]
-        pub struct $t;
-
         impl $t {
             #[doc = concat!("Obtain the number of times `", stringify!($t), "` can fit into `T`.")]
             #[doc = concat!("If `T` is smaller than `", stringify!($t), "`, the code will fail to")]
@@ -108,8 +113,19 @@ macro_rules! impl_per {
     )*};
 }
 
+declare_types! {
+    Nanosecond ("nanosecond")
+    Microsecond ("microsecond")
+    Millisecond ("millisecond")
+    Second ("second")
+    Minute ("minute")
+    Hour ("hour")
+    Day ("day")
+    Week ("week")
+}
+
 impl_per! {
-    Nanosecond ("nanosecond") per {
+    Nanosecond per {
         Nanosecond: [u8] u8|u16|u32|u64|u128|usize|i8|i16|i32|i64|i128|isize = 1; f32|f64 = 1.;
         Microsecond: [u16] u16|u32|u64|u128|usize|i16|i32|i64|i128|isize = 1_000; f32|f64 = 1_000.;
         Millisecond: [u32] u32|u64|u128|usize|i32|i64|i128|isize = 1_000_000; f32|f64 = 1_000_000.;
@@ -120,7 +136,7 @@ impl_per! {
         Day: [u64] u64|u128|i64|i128 = 86_400_000_000_000; f32|f64 = 86_400_000_000_000.;
         Week: [u64] u64|u128|i64|i128 = 604_800_000_000_000; f32|f64 = 604_800_000_000_000.;
     }
-    Microsecond ("microsecond") per {
+    Microsecond per {
         Microsecond: [u8] u8|u16|u32|u64|u128|usize|i8|i16|i32|i64|i128|isize = 1; f32|f64 = 1.;
         Millisecond: [u16] u16|u32|u64|u128|usize|i16|i32|i64|i128|isize = 1_000; f32|f64 = 1_000.;
         Second: [u32] u32|u64|u128|usize|i32|i64|i128|isize = 1_000_000; f32|f64 = 1_000_000.;
@@ -129,7 +145,7 @@ impl_per! {
         Day: [u64] u64|u128|i64|i128 = 86_400_000_000; f32|f64 = 86_400_000_000.;
         Week: [u64] u64|u128|i64|i128 = 604_800_000_000; f32|f64 = 604_800_000_000.;
     }
-    Millisecond ("millisecond") per {
+    Millisecond per {
         Millisecond: [u8] u8|u16|u32|u64|u128|usize|i8|i16|i32|i64|i128|isize = 1; f32|f64 = 1.;
         Second: [u16] u16|u32|u64|u128|usize|i16|i32|i64|i128|isize = 1_000; f32|f64 = 1_000.;
         Minute: [u16] u16|u32|u64|u128|usize|i32|i64|i128|isize = 60_000; f32|f64 = 60_000.;
@@ -137,29 +153,29 @@ impl_per! {
         Day: [u32] u32|u64|u128|usize|i32|i64|i128|isize = 86_400_000; f32|f64 = 86_400_000.;
         Week: [u32] u32|u64|u128|usize|i32|i64|i128|isize = 604_800_000; f32|f64 = 604_800_000.;
     }
-    Second ("second") per {
+    Second per {
         Second: [u8] u8|u16|u32|u64|u128|usize|i8|i16|i32|i64|i128|isize = 1; f32|f64 = 1.;
         Minute: [u8] u8|u16|u32|u64|u128|usize|i8|i16|i32|i64|i128|isize = 60; f32|f64 = 60.;
         Hour: [u16] u16|u32|u64|u128|usize|i16|i32|i64|i128|isize = 3_600; f32|f64 = 3_600.;
         Day: [u32] u32|u64|u128|usize|i32|i64|i128|isize = 86_400; f32|f64 = 86_400.;
         Week: [u32] u32|u64|u128|usize|i32|i64|i128|isize = 604_800; f32|f64 = 604_800.;
     }
-    Minute ("minute") per {
+    Minute per {
         Minute: [u8] u8|u16|u32|u64|u128|usize|i8|i16|i32|i64|i128|isize = 1; f32|f64 = 1.;
         Hour: [u8] u8|u16|u32|u64|u128|usize|i8|i16|i32|i64|i128|isize = 60; f32|f64 = 60.;
         Day: [u16] u16|u32|u64|u128|usize|i16|i32|i64|i128|isize = 1_440; f32|f64 = 1_440.;
         Week: [u16] u16|u32|u64|u128|usize|i16|i32|i64|i128|isize = 10_080; f32|f64 = 10_080.;
     }
-    Hour ("hour") per {
+    Hour per {
         Hour: [u8] u8|u16|u32|u64|u128|usize|i8|i16|i32|i64|i128|isize = 1; f32|f64 = 1.;
         Day: [u8] u8|u16|u32|u64|u128|usize|i8|i16|i32|i64|i128|isize = 24; f32|f64 = 24.;
         Week: [u8] u8|u16|u32|u64|u128|usize|i16|i32|i64|i128|isize = 168; f32|f64 = 168.;
     }
-    Day ("day") per {
+    Day per {
         Day: [u8] u8|u16|u32|u64|u128|usize|i8|i16|i32|i64|i128|isize = 1; f32|f64 = 1.;
         Week: [u8] u8|u16|u32|u64|u128|usize|i8|i16|i32|i64|i128|isize = 7; f32|f64 = 7.;
     }
-    Week ("week") per {
+    Week per {
         Week: [u8] u8|u16|u32|u64|u128|usize|i8|i16|i32|i64|i128|isize = 1; f32|f64 = 1.;
     }
 }
